9e432657b8327ea835796009d189d5369681cac5
chrisZip <kapitachristian2003@gmail.com>
2021-01-25 00:49:19 -0500

    Updated readme.md

---
+++ README.md
@@ -0,0 +1,31 @@
+# Readme Generator CLI appliciation with Node.js
+
+## Description
+A node.js application that uses user input from inquirer to generate a README.md file for user repository. The README.md file is created in the ReadMe folder.
+
+## Table of contents 
+* [Description](#Description)
+* [Table of contents](#Table of contents)
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Contributing](#contributing)
+* [Question](#license)
+
+## Installation
+```bash
+npm init -y
+```
+```bash
+npm install inquirer --save
+```
+## Usage
+Run below command in CLI at  the root of your project
+```bash
+node index.js 
+```
+## Contributing
+Please fork the repo, add your contribitions and send a pull request
+
+## Questions
+Please email me if having any question  : christiankapita2003@gmail.com
+
---
+++ index.js
@@ -0,0 +1,81 @@
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateReadme = require("./utils/generateReadme")
+const writeFileAsync = util.promisify(fs.writeFile);
+console.log("testing");
+//Prompt the user questions to populate the README.md
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "What is the project title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a brief description of your project: "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe the installation process if required: ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is this project usage for?"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Chose the appropriate license for this project: ",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Who are the contributors of this project?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Is there any test?"
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Please enter your GitHub username: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email: "
+        }
+    ]);
+} 
+
+// function to initialize program util.promisify
+  async function init() {
+    try {
+        // Ask user questions and generate responses
+        const answers = await promptUser();
+        const generateContent = generateReadme(answers);
+        // Write new README.md to ReadMe Folder 
+        await writeFileAsync('./ReadMe/README.md', generateContent);
+        console.log('Successfully wrote to README.md');
+    }   catch(err) {
+        console.log(err);
+    }
+  }
+  
+  init();  
